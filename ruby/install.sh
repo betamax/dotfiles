3e08a6baf0db80a6e936c7b5001c3ff0b8427fc2
Max Novakovic <max@maxnov.com>
2015-01-27 19:39:26 +0100

    Update the way rbenv is checked for

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-if test ! $(which rbenv)
-then
+if VERB="$( which rbenv )" 2> /dev/null; then
   echo "  Installing rbenv for you."
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
   . ~/.zshrc
 fi
 
-if test ! $(which ruby-build)
-then
+if VERB="$( which ruby-build )" 2> /dev/null; then
   echo "  Installing ruby-build for you."
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   . ~/.zshrc
