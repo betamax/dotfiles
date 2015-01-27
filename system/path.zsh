bea44a576ddd0ca84fa6e7292d6915e273b7f798
Max Novakovic <max@maxnov.com>
2015-01-27 18:04:57 +0100

    Remove Java stuff

--- system/path.zsh
+++ system/path.zsh
@@ -3,8 +3,6 @@ export PATH="/Users/Max/.rbenv/versions/2.0.0-p247/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export JAVA_HOME="$(/usr/libexec/java_home)"
-
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
 export AWS_RDS_HOME="/usr/local/Cellar/rds-command-line-tools/1.14.001/libexec"
