a146acc1b6e05fbf85410ebfe475afd66892f3ee
Max Novakovic <max@maxnov.com>
2015-01-27 19:00:53 +0100

    Reload ~/.zshrc after installing rbenv

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,10 +5,12 @@ then
   echo "  Installing rbenv for you."
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+  . ~/.zshrc
 fi
 
 if test ! $(which ruby-build)
 then
   echo "  Installing ruby-build for you."
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  . ~/.zshrc
 fi
