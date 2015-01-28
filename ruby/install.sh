a0482943fa3a69e126c6fd717d35e4e5bcab548c
Max Novakovic <max@maxnov.com>
2015-01-28 11:11:13 +0100

    Fix rbenv installer

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+command_exists () {
+  command -v "$1" >/dev/null 2>&1;
+}
 
-if VERB="$( which rbenv )" 2> /dev/null; then
+if ! command_exists rbenv; then
   echo "  Installing rbenv for you."
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
   . ~/.zshrc
 fi
 
-if VERB="$( which ruby-build )" 2> /dev/null; then
+if ! command_exists ruby-build; then
   echo "  Installing ruby-build for you."
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   . ~/.zshrc
