ca9f7f9db54b97cf26da768fb212a9d29d99206d
Max Novakovic <max@maxnov.com>
2015-01-28 11:33:51 +0100

    Add ls aliases for Linux based OSs

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,12 +1,19 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
+if [[ "$(uname)" == "Darwin" ]] && $(gls &>/dev/null); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+fi
+
+# ls aliases for Linux
+if [[ "$(uname)" == "Linux" ]]; then
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la='ls -A --color'
 fi
 
 alias pretty_json="python -mjson.tool"
