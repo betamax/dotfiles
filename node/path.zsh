2ac6ea9254949a1a0a28cb43b6342c59026a631a
Max Novakovic <max@maxnov.com>
2015-03-10 12:14:42 +0000

    Update node install to use nvm

---
+++ node/path.zsh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/share/npm/bin:$PATH"
+export NODE_PATH="/usr/local/share/npm/lib/node_modules:/usr/local/lib/node_modules:$NODE_PATH"
+
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
--- npm/path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH="/usr/local/share/npm/bin:$PATH"
-export NODE_PATH="/usr/local/share/npm/lib/node_modules:$NODE_PATH"
