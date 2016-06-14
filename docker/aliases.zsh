b9772d017c01510efeea5ed5b2b541102b8562b1
Max Novakovic <max@maxnov.com>
2016-06-14 13:12:25 +0200

    Call boot2docker shellinit

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,5 @@
+eval `boot2docker shellinit 2>/dev/null`
+
 alias dl='docker ps -l -q'
 alias ddelexited='docker rm -v `docker ps -a -q -f status=exited`'
 alias ddeldangling='docker rmi $(docker images -q -f dangling=true)'
